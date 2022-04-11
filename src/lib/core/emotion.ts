d8e61ed37f6d0cbcfb342e779ef33d87f02cca6f
elcharitas <jonathanirhodia@gmail.com>
2022-04-11 16:19:20 +0100

    feat: add custom emotion instance

---
+++ src/lib/core/emotion.ts
@@ -0,0 +1,13 @@
+import createEmotion from '@emotion/css/create-instance';
+import { theme } from '$lib/theme';
+
+export const {
+	cx,
+	css: system,
+	injectGlobal,
+	keyframes,
+	merge
+} = createEmotion({
+	key: theme?.config?.cssVarPrefix ?? 'chakra',
+	speedy: true
+});
