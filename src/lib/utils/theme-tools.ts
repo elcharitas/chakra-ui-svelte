62580ed49068aa630429fc9404b4e9ab981cec30
elcharitas <jonathanirhodia@gmail.com>
2022-04-11 21:29:31 +0100

    chore: refactor theme tools

--- src/lib/utils/theme-tools.ts
+++ src/lib/utils/theme-tools.ts
@@ -1,3 +1,29 @@
-export function mode() {}
-export function transparentize() {}
-export function darken() {}
+import type { Theme } from '$lib/theme';
+
+/**
+ * Returns the value based on current theme color mode.
+ *
+ * @param light
+ * @param dark
+ * @returns
+ */
+export const mode = (light: unknown, dark: unknown) => (props: { colorMode: 'light' | 'dark' }) =>
+	props.colorMode === 'dark' ? dark : light;
+
+/**
+ * Makes colors transparent
+ *
+ * @param color color to transform
+ * @param opacity opacity to apply from 0 to 1
+ * @returns
+ */
+export const transparentize = (color: string, opacity: number) => (theme: Theme) => {
+	return (
+		theme.colors[color]
+			?.replace(/^#/, '')
+			.replace(/([0-9a-f]{2})/g, '$1 ')
+			.trim() +
+		' ' +
+		opacity
+	);
+};
