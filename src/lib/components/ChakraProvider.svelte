102cacac1d4b83a831655fbc0d413dedd2945390
elcharitas <jonathanirhodia@gmail.com>
2022-04-11 16:20:10 +0100

    feat: add chakra provider component

---
+++ src/lib/components/ChakraProvider.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { toCSSVar } from '@chakra-ui/styled-system';
+    import { injectGlobal } from '$lib/core';
+    import { theme as baseTheme, themeStore } from '$lib/theme';
+
+    export let theme = {};
+
+    themeStore.set(theme);
+
+    injectGlobal({
+        'html, body': {
+            margin: 0,
+            padding: 0,
+        },
+        ':host, :root, [data-theme]': toCSSVar({
+            ...baseTheme,
+            ...theme
+        }).__cssVars
+    });
+</script>
+
+<slot />
