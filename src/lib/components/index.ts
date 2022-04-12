9d4b91c584eae3a9802a19572aff6f5f0898a842
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 14:53:08 +0100

    chore: add proper exports for new components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,6 @@
 export * from './buttons/index.js';
 export { default as Box } from './Box.svelte';
+export { default as Icon } from './Icon.svelte';
+export { default as Link } from './Link.svelte';
 export { default as Text } from './Text.svelte';
 export { default as CharaProvider } from './ChakraProvider.svelte';
