d463e583d28b6e80bd7ad681887e9a94783e8a53
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 14:51:06 +0100

    feat: add Icon component

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Box from './Box.svelte';
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core/index.js';
+
+	const events = eventsForward(current_component);
+	export let as = 'svg';
+</script>
+
+<Box {as} {events} {...$$props}>
+	<slot />
+</Box>
