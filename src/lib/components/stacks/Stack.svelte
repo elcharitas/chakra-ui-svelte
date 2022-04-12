82e34d587630b565b8e5cdc43221bc7cccdaf3dc
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 18:05:09 +0100

    feat: add Stack component

---
+++ src/lib/components/stacks/Stack.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Box from '$lib/components/Box.svelte';
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core/index.js';
+
+	const events = eventsForward(current_component);
+
+	export let spacing = 2;
+	export let orientation = 'horizontal';
+</script>
+
+<Box as="div" apply="Flex" {spacing} flexDirection={orientation} {events} {...$$props}>
+	<slot />
+</Box>
