c66b408a7cc848dede7e2a7881d675fc4e20eeb9
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 18:32:14 +0100

    chore: rename export

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -4,4 +4,4 @@ export { default as Box } from './Box.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as Link } from './Link.svelte';
 export { default as Text } from './Text.svelte';
-export { default as CharaProvider } from './ChakraProvider.svelte';
+export { default as ChakraProvider } from './ChakraProvider.svelte';
