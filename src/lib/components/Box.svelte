8a22fecc6994a699d96d64995643cd2e512178b1
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 18:02:56 +0100

    feat: add support for AsComponent

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,12 +1,21 @@
-<script>
+<script lang="ts">
 	import { current_component } from 'svelte/internal';
 	import { styled, eventsForward } from '$lib/core/index.js';
+	import type { SvelteComponent } from 'svelte';
 
 	export let events = eventsForward(current_component);
-	export let as = 'div';
-	export const apply = 'Box';
+	export let as: typeof SvelteComponent | string = 'div';
+	export const apply: string = 'Box';
 </script>
 
-<svelte:element this={as} use:styled={$$props} use:events>
-	<slot />
-</svelte:element>
+{#if typeof as === 'string'}
+	<svelte:element this={as} use:styled={$$props} use:events>
+		<slot />
+	</svelte:element>
+{/if}
+
+{#if typeof as !== 'string'}
+	<svelte:component this={as}>
+		<slot />
+	</svelte:component>
+{/if}
