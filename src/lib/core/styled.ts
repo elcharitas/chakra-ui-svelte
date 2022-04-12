6040cd00bcba33e5a131d89f7c04aaa1bd44c68d
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 06:27:08 +0100

    chore: update styled action for component styles

--- src/lib/core/styled.ts
+++ src/lib/core/styled.ts
@@ -1,11 +1,32 @@
 import { css, toCSSVar } from '@chakra-ui/styled-system';
 import { theme } from '$lib/theme';
-import { system } from './emotion';
+import { system, cx } from './emotion';
 
-export function styled(node: HTMLElement, props: unknown) {
+export function styled<T>(node: HTMLElement, props: T) {
 	function update(props) {
-		const generateCSS = css(props)(toCSSVar(theme));
-		const className = system(generateCSS);
+		const themeVars = toCSSVar(theme);
+		const componentStyles = {
+			colorScheme: props.colorScheme
+		};
+		const component = theme?.components[props.as];
+		if (typeof component === 'object') {
+			const { baseStyle, variants, sizes, defaultProps } = component;
+			const { variant = defaultProps?.variant, size = defaultProps?.size } = props;
+			const sizeStyle = sizes?.[size];
+			if (!componentStyles.colorScheme) {
+				componentStyles.colorScheme = defaultProps?.colorScheme;
+			}
+			Object.assign(componentStyles, {
+				...baseStyle,
+				...sizeStyle,
+				...variants?.[variant](componentStyles)
+			});
+		}
+		const generateCSS = css(props)(themeVars);
+		const componentCSS = css(componentStyles)(themeVars);
+		const baseName = system(generateCSS);
+		const componentClassName = system(componentCSS);
+		const className = cx(componentClassName, baseName, props.className);
 
 		node.classList.add(className);
 	}
