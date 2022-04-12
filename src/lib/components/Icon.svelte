76e875d3024904cb8f5af573a68d8cd03990a731
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 18:06:44 +0100

    feat: add support for components into icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,10 +1,11 @@
-<script>
+<script lang="ts">
 	import Box from './Box.svelte';
 	import { current_component } from 'svelte/internal';
 	import { eventsForward } from '$lib/core/index.js';
+	import type { SvelteComponent } from 'svelte';
 
 	const events = eventsForward(current_component);
-	export let as = 'svg';
+	export let as: typeof SvelteComponent | string = 'svg';
 </script>
 
 <Box {as} {events} {...$$props}>
