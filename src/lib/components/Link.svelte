089d485c3032e07be5fd183c6ee555dd904ca5b9
elcharitas <jonathanirhodia@gmail.com>
2022-04-12 14:47:29 +0100

    feat: add link component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Box from './Box.svelte';
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core/index.js';
+
+	const events = eventsForward(current_component);
+</script>
+
+<Box as="a" {events} {...$$props}>
+	<slot />
+</Box>
