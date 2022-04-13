c9ddf0b04f6723302f6123075bc28bbe0ae346e7
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 07:12:53 +0100

    chore: remove styles application

--- src/lib/components/stacks/Stack.svelte
+++ src/lib/components/stacks/Stack.svelte
@@ -1,4 +1,4 @@
-<script lang='ts'>
+<script lang="ts">
 	import Box from '$lib/components/Box.svelte';
 	import { current_component } from 'svelte/internal';
 	import { eventsForward } from '$lib/core/index.js';
@@ -9,6 +9,13 @@
 	export const orientation: 'horizontal' | 'vertical' = 'horizontal';
 </script>
 
-<Box as="div" apply="Flex" flexDirection={orientation} {events} {...$$props}>
+<Box
+	as="div"
+	display="flex"
+	flexDirection={orientation === 'horizontal' ? 'row' : 'column'}
+	flexWrap="wrap"
+	{events}
+	{...$$props}
+>
 	<slot />
 </Box>
