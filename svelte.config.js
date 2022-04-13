c602057ec94714d6507bfe14f6b5f90a131b0563
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 08:23:36 +0100

    feat: add sveltekit aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,22 @@
 import preprocess from 'svelte-preprocess';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess()
+	preprocess: preprocess(),
+	kit: {
+		vite: {
+			resolve: {
+				alias: {
+					$lib: resolve('./src/lib'),
+					$parts: resolve('./src/lib/components'),
+					$components: resolve('./src/components')
+				}
+			}
+		}
+	}
 };
 
 export default config;
