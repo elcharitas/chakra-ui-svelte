9ddf1dd4fbcf65f927290c71587cd5a17534b34d
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 07:01:11 +0100

    feat: add support for passed props

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-	import { current_component } from 'svelte/internal';
-	import { styled, eventsForward } from '$lib/core/index.js';
+	import { current_component, onMount } from 'svelte/internal';
+	import { eventsForward, styled } from '$lib/core/index.js';
 	import type { SvelteComponent } from 'svelte';
 
 	export let events = eventsForward(current_component);
 	export let as: typeof SvelteComponent | string = 'div';
+	export let props = {};
 	export const apply: string = 'Box';
+
+	let bound: EventTarget | SvelteComponent;
+	onMount(() => {
+		if (bound) {
+			events(bound as EventTarget);
+		}
+	});
 </script>
 
 {#if typeof as === 'string'}
-	<svelte:element this={as} use:styled={$$props} use:events>
+	<svelte:element this={as} use:styled={$$props} {...props} use:events>
 		<slot />
 	</svelte:element>
-{/if}
-
-{#if typeof as !== 'string'}
-	<svelte:component this={as}>
+{:else if typeof as !== 'string'}
+	<svelte:component this={as} bind:this={bound} on:click>
 		<slot />
 	</svelte:component>
 {/if}
