849f4de08c8db188a7d8a77a11cf3aeb28eef895
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 09:40:34 +0100

    feat: add IconButton component

---
+++ src/lib/components/buttons/IconButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core/index.js';
+	import Button from './Button.svelte';
+
+	export let icon;
+	const events = eventsForward(current_component);
+	const ButtonIcon = icon;
+</script>
+
+<Button {events} {...$$props}>
+	<ButtonIcon />
+</Button>
