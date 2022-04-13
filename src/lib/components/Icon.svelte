0ffa420107d8421a45a7d648efa532f7d2b22e11
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 07:05:27 +0100

    feat: add props and styles to Icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -6,8 +6,23 @@
 
 	const events = eventsForward(current_component);
 	export let as: typeof SvelteComponent | string = 'svg';
+
+	const styles = {
+		w: '1em',
+		h: '1em',
+		display: 'inline-block',
+		lineHeight: '1em',
+		flexShrink: 0,
+		color: 'currentColor'
+	};
+
+	const viewBox = '0 0 24 24';
 </script>
 
-<Box {as} {events} {...$$props}>
-	<slot />
-</Box>
+{#if typeof as === 'string'}
+	<Box {as} props={{ viewBox, ariaHidden: true }} {events} {...styles} {...$$props}>
+		<slot />
+	</Box>
+{:else if typeof as !== 'string'}
+	<Box {as} {events} {...styles} {...$$props} />
+{/if}
