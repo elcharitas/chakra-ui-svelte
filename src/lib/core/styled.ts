81c80eaa1d4b57a25480ed3257db8165167ec644
elcharitas <jonathanirhodia@gmail.com>
2022-04-13 06:55:55 +0100

    chore: break up styled action logic

--- src/lib/core/styled.ts
+++ src/lib/core/styled.ts
@@ -4,34 +4,37 @@ import { system, cx } from './emotion.js';
 
 export function styled<T>(node: HTMLElement, props: T) {
 	function update(props) {
-		const themeVars = toCSSVar(theme);
-		const componentStyles = {
-			colorScheme: props.colorScheme
-		};
-		const component = theme?.components[props.apply || props.as];
-		if (typeof component === 'object') {
-			const { baseStyle, variants, sizes, defaultProps } = component;
-			const { variant = defaultProps?.variant, size = defaultProps?.size } = props;
-			const sizeStyle = sizes?.[size];
-			if (!componentStyles.colorScheme) {
-				componentStyles.colorScheme = defaultProps?.colorScheme;
-			}
-			Object.assign(componentStyles, {
-				...baseStyle,
-				...sizeStyle,
-				...variants?.[variant](componentStyles)
-			});
-		}
-		const generateCSS = css(props)(themeVars);
-		const componentCSS = css(componentStyles)(themeVars);
-		const baseName = system(generateCSS);
-		const componentClassName = system(componentCSS);
-		const className = cx(componentClassName, baseName, props.className);
-
+		const className = createClass(props);
 		node.classList.add(className);
 	}
 
 	update(props);
 
 	return { update };
+}
+
+export function createClass(props) {
+	const themeVars = toCSSVar(theme);
+	const componentStyles = {
+		colorScheme: props.colorScheme
+	};
+	const component = theme?.components[props.apply || props.as];
+	if (typeof component === 'object') {
+		const { baseStyle, variants, sizes, defaultProps } = component;
+		const { variant = defaultProps?.variant, size = defaultProps?.size } = props;
+		const sizeStyle = sizes?.[size];
+		if (!componentStyles.colorScheme) {
+			componentStyles.colorScheme = defaultProps?.colorScheme;
+		}
+		Object.assign(componentStyles, {
+			...baseStyle,
+			...sizeStyle,
+			...variants?.[variant](componentStyles)
+		});
+	}
+	const baseCSS = css(props)(themeVars);
+	const componentCSS = css(componentStyles)(themeVars);
+	const baseName = system(baseCSS);
+	const componentClassName = system(componentCSS);
+	return cx(componentClassName, baseName);
 }
