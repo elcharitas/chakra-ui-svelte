0e0261c209b4904bbd6f06e8e69e5ffaaad3008e
elcharitas <jonathanirhodia@gmail.com>
2022-04-23 05:21:42 +0100

    feat: add mount checks to provider

--- src/lib/components/ChakraProvider.svelte
+++ src/lib/components/ChakraProvider.svelte
@@ -1,8 +1,10 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
 	import { toCSSVar } from '@chakra-ui/styled-system';
-	import { theme as baseTheme, themeStore, injectGlobal } from '$lib/index.js';
+	import { theme as baseTheme, colorMode, themeStore, injectGlobal } from '$lib/index.js';
 
 	export let theme = baseTheme;
+	export let isMounted = false;
 
 	themeStore.set(theme);
 
@@ -13,6 +15,16 @@
 		},
 		':host, :root, [data-theme]': toCSSVar(theme).__cssVars
 	});
+
+	onMount(() => {
+		const mode = localStorage.getItem('chakra-ui-color-mode') as any;
+		if (mode in ['light', 'dark']) {
+			colorMode.set(mode);
+		}
+		isMounted = true;
+	});
 </script>
 
-<slot />
+{#if isMounted}
+	<slot />
+{/if}
