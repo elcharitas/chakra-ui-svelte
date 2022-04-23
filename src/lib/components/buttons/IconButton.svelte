af3d5af88107f8640a0799a1d7b9da49cf932730
elcharitas <jonathanirhodia@gmail.com>
2022-04-23 13:06:54 +0100

    chore: fix icon resolution

--- src/lib/components/buttons/IconButton.svelte
+++ src/lib/components/buttons/IconButton.svelte
@@ -2,12 +2,12 @@
 	import { current_component } from 'svelte/internal';
 	import { eventsForward } from '$lib/core/index.js';
 	import Button from './Button.svelte';
+	import Icon from '$lib/components/basic/Icon.svelte';
 
 	export let icon;
 	const events = eventsForward(current_component);
-	const ButtonIcon = icon;
 </script>
 
 <Button {events} {...$$props}>
-	<ButtonIcon />
+	<Icon as={icon} />
 </Button>
