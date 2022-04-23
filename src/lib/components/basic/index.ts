06090547c62e39c2993a577f6f15f53e909d6eec
elcharitas <jonathanirhodia@gmail.com>
2022-04-23 14:54:16 +0100

    feat: add Logo component

--- src/lib/components/basic/index.ts
+++ src/lib/components/basic/index.ts
@@ -2,3 +2,4 @@ export { default as Box } from './Box.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as Link } from './Link.svelte';
 export { default as Text } from './Text.svelte';
+export { default as Logo } from './Logo.svelte';
