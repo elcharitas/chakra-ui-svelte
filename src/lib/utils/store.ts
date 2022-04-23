88f820392dd6d9f56669f8edd77d70a12ae70f5f
elcharitas <jonathanirhodia@gmail.com>
2022-04-23 05:39:43 +0100

    feat: add initial subscription call to stores

--- src/lib/utils/store.ts
+++ src/lib/utils/store.ts
@@ -5,18 +5,24 @@ type StoreCallback<T> = () => T;
 /**
  * Creates a custom store for use by Chakra UI Svelte
  *
- * @param key used to cache the value
- * @param initialValue
+ * @param initialValue - The initial value of the store
+ * @param subscription - A function that will be called when the store is updated
  * @returns
  */
-export function createStore<T>(initialValue: T | StoreCallback<T>) {
+export function createStore<T>(
+	initialValue: T | StoreCallback<T>,
+	subscription: (value: T) => void = () => null
+) {
 	if (initialValue instanceof Function) {
 		initialValue = initialValue();
 	}
 	const { subscribe, set, update } = writable(initialValue);
+
 	subscribe((val) => {
 		initialValue = val;
+		subscription(val);
 	});
+
 	return {
 		subscribe,
 		set,
