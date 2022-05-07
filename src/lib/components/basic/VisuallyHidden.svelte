8590fd40dce42ebf151d89ad2a1022a2fcf3b2cd
elcharitas <jonathanirhodia@gmail.com>
2022-05-07 08:24:34 +0000

    chore: add visually hidden component

---
+++ src/lib/components/basic/VisuallyHidden.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Box from './Box.svelte';
+	const visuallyHiddenStyle = {
+		border: '0px',
+		clip: 'rect(0px, 0px, 0px, 0px)',
+		height: '1px',
+		width: '1px',
+		margin: '-1px',
+		padding: '0px',
+		overflow: 'hidden',
+		whiteSpace: 'nowrap',
+		position: 'absolute'
+	};
+</script>
+
+<Box as="span" {...visuallyHiddenStyle} {...$$props}>
+	<slot />
+</Box>
