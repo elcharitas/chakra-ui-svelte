192144dde1fa996f15d5e127ac99da19c47c26ac
elcharitas <jonathanirhodia@gmail.com>
2022-05-24 12:29:32 +0100

    feat: add spinner component

---
+++ src/lib/components/anims/Spinner.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core/index.js';
+	import Box from '$lib/components/basic/Box.svelte';
+
+	const events = eventsForward(current_component);
+	export let color = 'grey';
+	export let size = 'md';
+</script>
+
+<Box
+	{size}
+	br="50%"
+	apply="Spinner"
+	border="16px solid"
+	borderColor={color}
+	animation="spin 2s linear infinite"
+	{events}
+	{...$$props}
+/>
