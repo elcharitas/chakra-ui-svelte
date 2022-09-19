64ed1bdd21433eed4141a428907bd89826c850e4
elcharitas <jonathanirhodia@gmail.com>
2022-09-19 02:08:41 +0100

    chore: move stories to new section

--- src/lib/components/anims/Spinner.stories.js
+++ src/lib/components/anims/Spinner.stories.js
@@ -1,7 +1,7 @@
 import Spinner from './Spinner.svelte';
 
 export default {
-	title: 'Component/Spinner',
+	title: 'Components/Spinner',
 	component: Spinner,
 	parameters: {
 		// More on Story layout: https://storybook.js.org/docs/svelte/configure/story-layout
