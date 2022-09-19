c5501522ee2a54abec4cebc05491e85f8a51dfc5
elcharitas <jonathanirhodia@gmail.com>
2022-09-19 03:45:44 +0100

    chore: export visually hidden

--- src/lib/components/basic/index.ts
+++ src/lib/components/basic/index.ts
@@ -3,3 +3,4 @@ export { default as Icon } from './Icon.svelte';
 export { default as Link } from './Link.svelte';
 export { default as Text } from './Text.svelte';
 export { default as Logo } from './Logo.svelte';
+export { default as VisuallyHidden } from './VisuallyHidden.svelte';
