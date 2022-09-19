09202e612785cc6e9237761de25b89731b14a7f1
elcharitas <jonathanirhodia@gmail.com>
2022-09-19 02:01:22 +0100

    fix: use package vite config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path = require('path');
+const { loadConfigFromFile, mergeConfig } = require('vite');
+
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: [
@@ -14,5 +17,16 @@ module.exports = {
 	},
 	features: {
 		storyStoreV7: true
+	},
+	async viteFinal(config) {
+		const { config: userConfig } = await loadConfigFromFile(
+			path.resolve(__dirname, '../vite.config.ts')
+		);
+
+		return mergeConfig(config, {
+			...userConfig,
+			// manually specify plugins to avoid conflict
+			plugins: []
+		});
 	}
 };
