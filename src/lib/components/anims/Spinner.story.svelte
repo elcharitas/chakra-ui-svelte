2d56e9d335caf02049d4d87762d35799e8d8f08f
elcharitas <jonathanirhodia@gmail.com>
2022-12-31 18:14:44 +0100

    chore: change stories from storybook

---
+++ src/lib/components/anims/Spinner.story.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Spinner from './Spinner.svelte';
+
+	export let Hst;
+</script>
+
+<Hst.Story id="anims/spinner">
+	<Hst.Variant>
+		<Spinner />
+	</Hst.Variant>
+</Hst.Story>
