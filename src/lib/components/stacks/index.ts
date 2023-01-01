14dbbc0f48073addfba0a6c5266937cc275ba2dd
elcharitas <jonathanirhodia@gmail.com>
2023-01-01 09:41:42 +0100

    chore: export flex component

--- src/lib/components/stacks/index.ts
+++ src/lib/components/stacks/index.ts
@@ -1,3 +1,4 @@
 export { default as HStack } from './HStack.svelte';
 export { default as VStack } from './VStack.svelte';
 export { default as Stack } from './Stack.svelte';
+export { default as Flex } from './Flex.svelte';
