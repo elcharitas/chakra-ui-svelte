890080c313a784f4694e7d3e2335c54281b47fa0
elcharitas <jonathanirhodia@gmail.com>
2023-02-24 23:16:35 +0100

    chore: drop custom emotion instance.

--- src/lib/core/emotion.ts
+++ src/lib/core/emotion.ts
@@ -1,7 +1,1 @@
-import createEmotion from '@emotion/css/create-instance';
-import { theme } from '$lib/theme';
-
-export const { cx, css: system, injectGlobal, keyframes, merge } = createEmotion({
-	key: theme?.config?.cssVarPrefix ?? 'chakra',
-	speedy: true
-});
+export { cx, css as system, injectGlobal } from '@emotion/css';
