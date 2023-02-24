3687ce1e3dc9daf42f16a99117de18f0bb1506c2
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-02-24 23:45:03 +0100

    feat: Add support for theme provider (#26)
    * feat: create global styles
    
    * feat: deprecate `ChakraProvider` for `ThemeProvider`

---
+++ src/lib/components/providers/GlobalStyles.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { toCSSVar } from '@chakra-ui/styled-system';
+	import { themeStore } from '$lib/theme';
+	import { injectGlobal } from '$lib/core';
+
+	export let styles = {};
+
+	injectGlobal({
+		'html, body': {
+			margin: 0,
+			padding: 0
+		},
+		':host, :root, [data-theme]': toCSSVar($themeStore).__cssVars,
+		...styles
+	});
+</script>
--- src/lib/components/providers/index.ts
+++ src/lib/components/providers/index.ts
@@ -1,1 +1,4 @@
-export { default as ChakraProvider } from './ChakraProvider.svelte';
+/** @deprecated */
+export { default as ChakraProvider } from './ThemeProvider.svelte'; // backwards compatibility
+export { default as ThemeProvider } from './ThemeProvider.svelte';
+export { default as GlobalStyles } from './GlobalStyles.svelte';
