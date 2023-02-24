bff3958e42ee6e56db268780247862b7001ef4df
elcharitas <jonathanirhodia@gmail.com>
2023-02-24 23:48:42 +0100

    chore: cleanup and apply styles

--- src/lib/components/basic/Icon.svelte
+++ src/lib/components/basic/Icon.svelte
@@ -5,12 +5,8 @@
 
 	const events = eventsForward(current_component);
 	export let as = 'svg';
-	export let viewBox = undefined;
-	export let width = undefined;
-	export let height = undefined;
-	export let xmlns = undefined;
 </script>
 
-<Box {as} {events} sx={{ width: '20px' }} props={{ viewBox, width, height, xmlns }} {...$$props}>
+<Box {as} {events} apply="Icon" size="sm" {...$$props}>
 	<slot />
 </Box>
