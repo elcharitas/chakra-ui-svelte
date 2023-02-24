48fab1c4e84e993b1beec2a428c1e4ab39405588
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-02-24 23:12:18 +0100

    feat: add container component (#24)

---
+++ src/lib/components/basic/Container.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { current_component } from 'svelte/internal';
+	import { eventsForward } from '$lib/core';
+	import Box from './Box.svelte';
+
+	const events = eventsForward(current_component);
+</script>
+
+<Box {events} mx="auto" {...$$props}>
+	<slot />
+</Box>
