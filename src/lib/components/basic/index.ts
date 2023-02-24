7dedb8fc09e456b813f58267ea774e0d21822b7e
elcharitas <jonathanirhodia@gmail.com>
2023-02-24 23:10:29 +0100

    feat: add container component

--- src/lib/components/basic/index.ts
+++ src/lib/components/basic/index.ts
@@ -4,3 +4,4 @@ export { default as Link } from './Link.svelte';
 export { default as Text } from './Text.svelte';
 export { default as Logo } from './Logo.svelte';
 export { default as VisuallyHidden } from './VisuallyHidden.svelte';
+export { default as Container } from './Container.svelte';
