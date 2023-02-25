97865c6f1631f3f0f3ed8e1b598dfe76418ff6ea
Jonathan Irhodia <jonathanirhodia@gmail.com>
2023-02-25 13:49:49 +0100

    fix(ui): pass no slots for input component (#36)

--- src/lib/components/basic/Box.svelte
+++ src/lib/components/basic/Box.svelte
@@ -8,6 +8,7 @@
 	export let wrap: boolean | string = false;
 	export let props = {};
 	export let sx = {};
+	export let noSlot = false;
 
 	const styles = createStyle({ sx, ...props });
 	// this provides a fine grained control over the component's props
@@ -15,9 +16,13 @@
 </script>
 
 {#if typeof as === 'string'}
-	<svelte:element this={as} use:chakra={$$props} use:events use:attributes>
-		<slot />
-	</svelte:element>
+	{#if noSlot}
+		<svelte:element this={as} use:chakra={$$props} use:events use:attributes />
+	{:else}
+		<svelte:element this={as} use:chakra={$$props} use:events use:attributes>
+			<slot />
+		</svelte:element>
+	{/if}
 {:else if typeof as !== 'string'}
 	{#if wrap}
 		<svelte:element
